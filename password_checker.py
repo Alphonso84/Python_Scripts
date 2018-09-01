7fe751d41ca2b47d9fed5d21bc31e22fcbc4734c
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2018-09-01 15:16:27 -0700

    learning control flow statements

---
+++ password_checker.py
@@ -0,0 +1,10 @@
+import sys
+
+password = input("Please enter a password:        ")
+attempt_count = 1
+while password != '12345':
+    if attempt_count > 3:
+        sys.exit("Too many Invalid Attempts")
+    password = input("Invalid Password. Try Again:     ")
+    attempt_count += 1
+print("Password Correct!")
