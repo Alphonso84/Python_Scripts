1b20d53fbc1aa40477a12dc7bd5e550a7cbf73a5
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2018-09-01 15:32:25 -0700

    added password string as constant(ALL CAPS FOR CONSTANTS)

--- password_checker.py
+++ password_checker.py
@@ -1,9 +1,10 @@
 import sys
 
+MASTER_PASSWORD = '12345'
 password = input("Please enter a password:        ")
 attempt_count = 1
 
-while password != '12345':
+while password != MASTER_PASSWORD:
     if attempt_count > 3:
         sys.exit("Too many Invalid Attempts")
     password = input("Invalid Password. Try Again:     ")
