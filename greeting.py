c0a6154d23835621eb9b101f1716019cba5bfff6
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2018-11-29 21:36:02 -0800

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -4,4 +4,4 @@ name = input("What is your name? ")
 
 status = input("Hello {}, how are you today? ".format(name))
 
-print("Good to hear you're {} today!".format(status))
+print("Good to hear you're {} today".format(status))
